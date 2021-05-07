0e95fd681fd049a5e0a930620fc98008535a8fa3
Arthur Melkonyan <arthur@artmelkon.me>
2021-05-06 20:38:51 -0700

    commiting files for github pages

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.style.css'
 
 export const Card = props => (
   <div className='card-container'>
-    <img alt="monster" src={`https:robohash.org/${props.monster.id}?set=set2&size=180x180`} />
+    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
     <h3>{props.monster.name}</h3>
     <p>{props.monster.email}</p>
     <p style={{fontSize: "10px"}}>Company: {props.monster.company.name}</p>
